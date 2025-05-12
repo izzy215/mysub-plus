d4a529ee5e9829785fcdba32a380a09ccfd8dd76
izzy215 <jihyeon3@gmail.com>
2025-05-12 22:57:56 +0900

    라우팅 보호 : 로그인된사용자 대시보드 접근

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,27 @@
+//라우팅 보호
+
+'use client';
+
+import { ReactNode, useEffect } from 'react';
+import { useAuth } from '@/context/AuthContext';
+import { useRouter } from 'next/navigation';
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+//isAuthenticated 없으면 /login 으로 redirect
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      //isAuthenticated 없으면 /login 으로 redirect
+      router.push('/login');
+    }
+  }, [isAuthenticated, router]);
+  
+  //isAuthenticated 있으면 렌더링 없으며 null 
+  return <>{isAuthenticated ? children : null}</>;
+}
